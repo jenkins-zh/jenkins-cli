e25c8531432cc1dc5538c866e3f61200d7086d2e
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-07-02 13:55:52 +0800

    Add batch mode option

--- app/cmd/restart.go
+++ app/cmd/restart.go
@@ -11,8 +11,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// RestartOption holds the options for restart cmd
+type RestartOption struct {
+	BatchOption
+}
+
+var restartOption RestartOption
+
 func init() {
 	rootCmd.AddCommand(restartCmd)
+	restartCmd.Flags().BoolVarP(&restartOption.Batch, "batch", "b", false, "Batch mode, no need confirm")
 }
 
 var restartCmd = &cobra.Command{
@@ -21,13 +29,16 @@ var restartCmd = &cobra.Command{
 	Long:  `Restart your Jenkins`,
 	Run: func(cmd *cobra.Command, args []string) {
 		crumb, config := getCrumb()
-		confirm := false
-		prompt := &survey.Confirm{
-			Message: fmt.Sprintf("Are you sure to restart Jenkins %s?", config.URL),
-		}
-		survey.AskOne(prompt, &confirm)
-		if !confirm {
-			return
+
+		if !restartOption.Batch {
+			confirm := false
+			prompt := &survey.Confirm{
+				Message: fmt.Sprintf("Are you sure to restart Jenkins %s?", config.URL),
+			}
+			survey.AskOne(prompt, &confirm)
+			if !confirm {
+				return
+			}
 		}
 
 		api := fmt.Sprintf("%s/safeRestart", config.URL)
