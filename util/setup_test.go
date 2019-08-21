02c799401babb3922b27c626aace70e19e1b0acd
Rick <rick@jenkins-zh.cn>
2019-08-21 18:57:32 +0800

    Add test cases for password util

---
+++ util/setup_test.go
@@ -0,0 +1,13 @@
+package util
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestUtils(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "jcli utils test")
+}
