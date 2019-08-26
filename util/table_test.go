ce453dff97006e1757a87edd55c09c37b3216b05
Rick <rick@jenkins-zh.cn>
2019-08-26 08:29:29 +0800

    Fix the test cases

--- util/table_test.go
+++ util/table_test.go
@@ -1,7 +1,8 @@
 package util
 
 import (
-	"os"
+	"bytes"
+	"strings"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
@@ -23,24 +24,22 @@ var _ = Describe("Table util test", func() {
 
 	Context("basic test", func() {
 		It("test", func() {
-			table := CreateTable(os.Stdout)
+			var buffer bytes.Buffer
+			table := CreateTable(&buffer)
 			table.AddRow("number", "name", "type")
-			table.AddRow("0 ZSOMZAYNHG Standalone Projects")
-			table.AddRow("0 构建一个自由风格的软件项 Standalone Projects")
-			table.AddRow("0 建置 Free-Style 軟體專案 Standalone Projects")
-			table.AddRow("0 フリースタイル・プロジェクトのビルド Standalone Projects")
-			table.AddRow("0 フリースタイル・プ中ロジasdェクトのビル Standalone Projects")
+			table.AddRow("0", "ZSOMZAYNHG", "Standalone Projects")
+			table.AddRow("1", "构建一个自由风格的软件项", "Standalone Projects")
+			table.AddRow("2", "建置 Free-Style 軟體專案", "Standalone Projects")
+			table.AddRow("3", "フリースタイル・プロジェクトのビルド", "Standalone Projects")
+			table.AddRow("4", "フリースタイル・プ中ロジasdェクトのビル", "Standalone Projects")
 			table.Render()
-			table1 := CreateTable(os.Stdout)
-			table1.AddRow(`
-			number name                                type
-			0      ZSOMZAYNHG                          Standalone Projects
-			1      构建一个自由风格的软件项                Standalone Projects
-			2      建置 Free-Style 軟體專案              Standalone Projects
-			3      フリースタイル・プロジェクトのビルド      Standalone Projects
-			4      フリースタイル・プ中ロジasdェクトのビル   Standalone Projects
-			`)
-			Expect(table.Out).To(Equal(table1.Out))
+			Expect(strings.TrimSpace(`
+number name                                type
+0      ZSOMZAYNHG                          Standalone Projects
+1      构建一个自由风格的软件项                Standalone Projects
+2      建置 Free-Style 軟體專案              Standalone Projects
+3      フリースタイル・プロジェクトのビルド      Standalone Projects
+4      フリースタイル・プ中ロジasdェクトのビル   Standalone Projects`)).To(Equal(buffer.String()))
 		})
 	})
 })
