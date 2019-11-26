93d75bbc13e11b66f123ce9dcd47c67a7197c198
rick <rick@jenkins-zh.cn>
2019-11-26 21:55:16 +0800

    Use syscal instead of execute

--- app/cmd/plugin_create.go
+++ app/cmd/plugin_create.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"os/exec"
+	"syscall"
 
 	"github.com/jenkins-zh/jenkins-cli/app/i18n"
 
@@ -19,11 +20,11 @@ var pluginCreateCmd = &cobra.Command{
 	Long: i18n.T(`Create a plugin project from the archetypes
 Plugin tutorial is here https://jenkins.io/doc/developer/tutorial/`),
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		e := exec.Command("mvn", "-U", "archetype:generate", `-Dfilter="io.jenkins.archetypes:"`)
-		e.Stdout = cmd.OutOrStdout()
-		e.Stderr = cmd.OutOrStderr()
-		e.Stdin = os.Stdin
-		err = e.Run()
+		binary, err := exec.LookPath("mvn")
+		if err == nil {
+			env := os.Environ()
+			err = syscall.Exec(binary, []string{"-U", "archetype:generate", `-Dfilter="io.jenkins.archetypes:"`}, env)
+		}
 		return
 	},
 }
