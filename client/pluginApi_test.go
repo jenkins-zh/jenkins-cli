671bfc1c9021c9ec063464ffcd0c78be32fee8f0
yJunS <ko_no1@2008.sina.com>
2019-12-01 23:28:50 +0800

    Update unit test coverage

--- client/pluginApi_test.go
+++ client/pluginApi_test.go
@@ -133,5 +133,14 @@ var _ = Describe("plugin api test", func() {
 
 			defer os.Remove("fake.hpi")
 		})
+
+		It("batch search plugins", func() {
+			names = []string{"fake"}
+
+			PrepareShowPlugins(roundTripper, "fake")
+
+			plugins, _ := pluginAPI.BatchSearchPlugins("fake")
+			Expect(len(plugins)).To(Equal(0))
+		})
 	})
 })
