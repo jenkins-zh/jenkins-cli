e5e06365376801758de1bad5d2c2cf3ba3cb47d3
rick <rick@jenkins-zh.cn>
2019-12-19 17:59:20 +0800

    Improve list output of config list

--- app/cmd/config_list.go
+++ app/cmd/config_list.go
@@ -1,62 +1,53 @@
 package cmd
 
 import (
-	"bytes"
 	"fmt"
-
-	"github.com/jenkins-zh/jenkins-cli/app/helper"
 	"github.com/jenkins-zh/jenkins-cli/app/i18n"
 
-	"github.com/jenkins-zh/jenkins-cli/util"
 	"github.com/spf13/cobra"
 )
 
 // ConfigListOption option for config list command
 type ConfigListOption struct {
 	OutputOption
+
+	Config string
 }
 
 var configListOption ConfigListOption
 
 func init() {
 	configCmd.AddCommand(configListCmd)
-	configListOption.SetFlag(configListCmd)
+	configListCmd.Flags().StringVarP(&configListOption.Config, "config", "", "JenkinsServers",
+		i18n.T("The type of config items, contains PreHooks, PostHooks, Mirrors, PluginSuites"))
+	configListOption.SetFlagWithHeaders(configListCmd, "Name,URL,Description")
 }
 
 var configListCmd = &cobra.Command{
 	Use:   "list",
 	Short: i18n.T("List all Jenkins config items"),
 	Long:  i18n.T("List all Jenkins config items"),
-	Run: func(cmd *cobra.Command, _ []string) {
-		current := getCurrentJenkins()
+	RunE: func(cmd *cobra.Command, _ []string) (err error) {
+		configListOption.Writer = cmd.OutOrStdout()
 
-		data, err := configListOption.Output(current)
-		cmd.Print(string(data))
-		helper.CheckErr(cmd, err)
-	},
-}
-
-// Output render data into byte array as a table format
-func (o *ConfigListOption) Output(obj interface{}) (data []byte, err error) {
-	if data, err = o.OutputOption.Output(obj); err != nil && o.Format == TableOutputFormat {
-		current := obj.(*JenkinsServer)
-
-		buf := new(bytes.Buffer)
-		table := util.CreateTableWithHeader(buf, o.WithoutHeaders)
-		table.AddHeader("number", "name", "url", "description")
-		for i, jenkins := range getConfig().JenkinsServers {
-			name := jenkins.Name
-			if name == current.Name {
-				name = fmt.Sprintf("*%s", name)
-			}
-			if len(jenkins.Description) > 15 {
-				jenkins.Description = jenkins.Description[0:15]
-			}
-			table.AddRow(fmt.Sprintf("%d", i), name, jenkins.URL, jenkins.Description)
+		switch configListOption.Config {
+		case "JenkinsServers":
+			err = configListOption.OutputV2(getConfig().JenkinsServers)
+		case "PreHooks":
+			configListOption.Columns = "Path,Command"
+			err = configListOption.OutputV2(getConfig().PreHooks)
+		case "PostHooks":
+			configListOption.Columns = "Path,Command"
+			err = configListOption.OutputV2(getConfig().PostHooks)
+		case "Mirrors":
+			configListOption.Columns = "Name,URL"
+			err = configListOption.OutputV2(getConfig().Mirrors)
+		case "PluginSuites":
+			configListOption.Columns = "Name,Description"
+			err = configListOption.OutputV2(getConfig().PluginSuites)
+		default:
+			err = fmt.Errorf("unknow config %s", configListOption.Config)
 		}
-		table.Render()
-		err = nil
-		data = buf.Bytes()
-	}
-	return
+		return
+	},
 }
--- app/cmd/config_list_test.go
+++ app/cmd/config_list_test.go
@@ -25,6 +25,7 @@ var _ = Describe("config list command", func() {
 	})
 
 	AfterEach(func() {
+		config = nil
 		rootCmd.SetArgs([]string{})
 		os.Remove(rootOptions.ConfigFile)
 		rootOptions.ConfigFile = ""
@@ -43,8 +44,8 @@ var _ = Describe("config list command", func() {
 			rootCmd.SetArgs([]string{"config", "list"})
 			_, err = rootCmd.ExecuteC()
 			Expect(err).To(BeNil())
-			Expect(buf.String()).To(Equal(`number name        url                           description
-0      *yourServer http://localhost:8080/jenkins 
+			Expect(buf.String()).To(Equal(`Name       URL                           Description
+yourServer http://localhost:8080/jenkins 
 `))
 		})
 
@@ -61,9 +62,77 @@ var _ = Describe("config list command", func() {
 			rootCmd.SetArgs([]string{"config", "list"})
 			_, err = rootCmd.ExecuteC()
 			Expect(err).To(BeNil())
-			Expect(buf.String()).To(Equal(`number name        url                           description
-0      *yourServer http://localhost:8080/jenkins 012345678901234
+			Expect(buf.String()).To(Equal(`Name       URL                           Description
+yourServer http://localhost:8080/jenkins 01234567890123456789
 `))
+		})
+
+		It("print the list of PreHooks", func() {
+			sampleConfig := getSampleConfig()
+			config = &sampleConfig
+
+			buf := new(bytes.Buffer)
+			rootCmd.SetOutput(buf)
+			rootCmd.SetArgs([]string{"config", "list", "--config", "PreHooks"})
+			_, err := rootCmd.ExecuteC()
+			Expect(err).To(BeNil())
+			Expect(buf.String()).To(Equal(`Path Command
+`))
+		})
+
+		It("print the list of PostHooks", func() {
+			sampleConfig := getSampleConfig()
+			config = &sampleConfig
+
+			buf := new(bytes.Buffer)
+			rootCmd.SetOutput(buf)
+			rootCmd.SetArgs([]string{"config", "list", "--config", "PostHooks"})
+			_, err := rootCmd.ExecuteC()
+			Expect(err).To(BeNil())
+			Expect(buf.String()).To(Equal(`Path Command
+`))
+		})
+
+		It("print the list of Mirrors", func() {
+			sampleConfig := getSampleConfig()
+			config = &sampleConfig
+
+			buf := new(bytes.Buffer)
+			rootCmd.SetOutput(buf)
+			rootCmd.SetArgs([]string{"config", "list", "--config", "Mirrors"})
+			_, err := rootCmd.ExecuteC()
+			Expect(err).To(BeNil())
+			Expect(buf.String()).To(Equal(`Name     URL
+default  http://mirrors.jenkins.io/
+tsinghua https://mirrors.tuna.tsinghua.edu.cn/jenkins/
+huawei   https://mirrors.huaweicloud.com/jenkins/
+tencent  https://mirrors.cloud.tencent.com/jenkins/
+`))
+		})
+
+		It("print the list of PluginSuites", func() {
+			sampleConfig := getSampleConfig()
+			config = &sampleConfig
+
+			buf := new(bytes.Buffer)
+			rootCmd.SetOutput(buf)
+			rootCmd.SetArgs([]string{"config", "list", "--config", "PluginSuites"})
+			_, err := rootCmd.ExecuteC()
+			Expect(err).To(BeNil())
+			Expect(buf.String()).To(Equal(`Name Description
+`))
+		})
+
+		It("print the list of a fake type", func() {
+			sampleConfig := getSampleConfig()
+			config = &sampleConfig
+
+			buf := new(bytes.Buffer)
+			rootCmd.SetOutput(buf)
+			rootCmd.SetArgs([]string{"config", "list", "--config", "fake"})
+			_, err := rootCmd.ExecuteC()
+			Expect(err).To(HaveOccurred())
+			Expect(buf.String()).To(ContainSubstring("unknow config"))
 		})
 	})
 })
