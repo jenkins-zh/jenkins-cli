0f300927ff3a069d5fd0403b395feaaf5537734e
Zhao Xiaojie <rick@jenkins-zh.cn>
2019-12-23 09:28:37 +0800

    Add support get the identity of Jenkins (#292)
    * Add support get the identity of Jenkins
    
    * Add an item on the pull request template

---
+++ app/cmd/center_identity_test.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"bytes"
+	"github.com/golang/mock/gomock"
+	"github.com/jenkins-zh/jenkins-cli/client"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"os"
+
+	"github.com/jenkins-zh/jenkins-cli/mock/mhttp"
+)
+
+var _ = Describe("center identity command", func() {
+	var (
+		ctrl           *gomock.Controller
+		roundTripper   *mhttp.MockRoundTripper
+		targetFilePath string
+
+		err error
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		roundTripper = mhttp.NewMockRoundTripper(ctrl)
+		centerIdentityOption.RoundTripper = roundTripper
+		targetFilePath = "jenkins.war"
+
+		rootOptions.Jenkins = ""
+		rootOptions.ConfigFile = "test.yaml"
+	})
+
+	AfterEach(func() {
+		rootCmd.SetArgs([]string{})
+		err = os.Remove(targetFilePath)
+		ctrl.Finish()
+	})
+
+	Context("basic cases", func() {
+		It("should not error", func() {
+			var data []byte
+			data, err = generateSampleConfig()
+			Expect(err).To(BeNil())
+			err = ioutil.WriteFile(rootOptions.ConfigFile, data, 0664)
+			Expect(err).To(BeNil())
+
+			client.PrepareForGetIdentity(roundTripper, "http://localhost:8080/jenkins",
+				"admin", "111e3a2f0231198855dceaff96f20540a9")
+
+			buf := new(bytes.Buffer)
+			rootCmd.SetOut(buf)
+			rootCmd.SetArgs([]string{"center", "identity"})
+			_, err = rootCmd.ExecuteC()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(buf.String()).To(Equal(`{
+ "Fingerprint": "fingerprint",
+ "PublicKey": "publicKey",
+ "SystemMessage": "systemMessage"
+}
+`))
+		})
+	})
+})
--- app/cmd/center_upgrade.go
+++ app/cmd/center_upgrade.go
@@ -25,20 +25,12 @@ var centerUpgradeCmd = &cobra.Command{
 	Short: i18n.T("Upgrade your Jenkins"),
 	Long:  i18n.T("Upgrade your Jenkins"),
 	RunE: func(cmd *cobra.Command, _ []string) (err error) {
-		jenkins := getCurrentJenkinsFromOptionsOrDie()
-
 		jclient := &client.UpdateCenterManager{
 			JenkinsCore: client.JenkinsCore{
 				RoundTripper: centerUpgradeOption.RoundTripper,
 			},
 		}
-		jclient.URL = jenkins.URL
-		jclient.UserName = jenkins.UserName
-		jclient.Token = jenkins.Token
-		jclient.Proxy = jenkins.Proxy
-		jclient.ProxyAuth = jenkins.ProxyAuth
-
-		err = jclient.Upgrade()
-		return
+		getCurrentJenkinsAndClient(&(jclient.JenkinsCore))
+		return jclient.Upgrade()
 	},
 }
