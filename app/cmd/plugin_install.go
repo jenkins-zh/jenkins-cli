c6784c03ec04df15d0c4395f47d3b43dd07ba23c
rick <rick@jenkins-zh.cn>
2020-11-18 17:32:45 +0800

    Add support to install plugins via Jenkins formula

--- app/cmd/plugin_install.go
+++ app/cmd/plugin_install.go
@@ -2,12 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/jenkins-zh/jenkins-cli/app/helper"
+	"github.com/ghodss/yaml"
 	"github.com/jenkins-zh/jenkins-cli/app/i18n"
+	"go.uber.org/zap"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/jenkins-zh/jenkins-cli/client"
+	jenkinsFormula "github.com/jenkins-zh/jenkins-formulas/pkg/common"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +18,7 @@ import (
 type PluginInstallOption struct {
 	UseMirror    bool
 	ShowProgress bool
+	Formula      string
 
 	RoundTripper http.RoundTripper
 }
@@ -23,22 +27,26 @@ var pluginInstallOption PluginInstallOption
 
 func init() {
 	pluginCmd.AddCommand(pluginInstallCmd)
-	pluginInstallCmd.Flags().BoolVarP(&pluginInstallOption.UseMirror, "use-mirror", "", true,
+
+	flags := pluginInstallCmd.Flags()
+	flags.BoolVarP(&pluginInstallOption.UseMirror, "use-mirror", "", true,
 		i18n.T("If you want to download plugin from a mirror site"))
-	pluginInstallCmd.Flags().BoolVarP(&pluginInstallOption.ShowProgress, "show-progress", "", true,
+	flags.BoolVarP(&pluginInstallOption.ShowProgress, "show-progress", "", true,
 		i18n.T("If you want to show the progress of download a plugin"))
-	pluginInstallCmd.Flags().StringVarP(&pluginOpt.Suite, "suite", "", "", "Suite of plugins")
+	flags.StringVarP(&pluginOpt.Suite, "suite", "", "", "Suite of plugins")
+	flags.StringVarP(&pluginInstallOption.Formula, "formula", "", "",
+		"Install plugins via a Jenkins formula. If you want to know more about Jenkins formula, please checkout https://github.com/jenkinsci/custom-war-packager")
 }
 
 var pluginInstallCmd = &cobra.Command{
-	Use:   "install [pluginName]",
+	Use:   "install",
 	Short: i18n.T("Install the plugins"),
 	Long: i18n.T(`Install the plugins
 Allow you to install a plugin with or without the version`),
 	Example: `jcli plugin install localization-zh-cn
 jcli plugin install localization-zh-cn@1.0.9
 `,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		jclient := &client.PluginManager{
 			JenkinsCore: client.JenkinsCore{
 				RoundTripper: pluginInstallOption.RoundTripper,
@@ -52,12 +60,28 @@ jcli plugin install localization-zh-cn@1.0.9
 		plugins := make([]string, len(args))
 		plugins = append(plugins, args...)
 
-		var err error
 		if pluginOpt.Suite != "" {
 			if suite := findSuiteByName(pluginOpt.Suite); suite != nil {
 				plugins = append(plugins, suite.Plugins...)
 			} else {
 				err = fmt.Errorf("cannot found suite %s", pluginOpt.Suite)
+			}
+		}
+
+		if pluginInstallOption.Formula != "" {
+			var data []byte
+			if data, err = ioutil.ReadFile(pluginInstallOption.Formula); err == nil {
+				formula := jenkinsFormula.CustomWarPackage{}
+				if err = yaml.Unmarshal(data, &formula); err == nil {
+					for _, val := range formula.Plugins {
+						plugins = append(plugins, val.ArtifactId)
+					}
+					logger.Info("prepare to install plugins", zap.Int("count", len(plugins)))
+				}
+			}
+
+			if err != nil {
+				err = fmt.Errorf("cannot read the Jenkins formular. %v", err)
 			}
 		}
 
@@ -98,7 +122,7 @@ jcli plugin install localization-zh-cn@1.0.9
 		if err == nil {
 			err = jclient.InstallPlugin(plugins)
 		}
-		helper.CheckErr(cmd, err)
+		return
 	},
 }
 
