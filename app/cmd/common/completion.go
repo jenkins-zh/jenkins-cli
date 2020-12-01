2637633e00f1a2d15f7e656ed7de928e5ceaeb53
rick <rick@jenkins-zh.cn>
2020-12-01 15:16:30 +0800

    Support run Jenkins agent as docker

--- app/cmd/common/completion.go
+++ app/cmd/common/completion.go
@@ -6,3 +6,10 @@ import "github.com/spf13/cobra"
 func NoFileCompletion(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 	return nil, cobra.ShellCompDirectiveNoFileComp
 }
+
+// ArrayCompletion return a completion  which base on an array
+func ArrayCompletion(array ...string) func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	return func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return array, cobra.ShellCompDirectiveNoFileComp
+	}
+}
--- app/cmd/computer_launch.go
+++ app/cmd/computer_launch.go
@@ -26,6 +26,12 @@ type ComputerLaunchOption struct {
 	ComputerClient *client.ComputerClient
 	CurrentJenkins *appCfg.JenkinsServer
 	Output         string
+
+	Mode      string
+	Remove    bool
+	Restart   string
+	Detach    bool
+	AgentType string
 }
 
 const (
@@ -37,10 +43,33 @@ var computerLaunchOption ComputerLaunchOption
 
 func init() {
 	computerCmd.AddCommand(computerLaunchCmd)
-	computerLaunchCmd.Flags().StringVarP(&computerLaunchOption.Type, "type", "", AgentJNLP,
+	flags := computerLaunchCmd.Flags()
+
+	flags.StringVarP(&computerLaunchOption.Mode, "mode", "m", "java",
+		i18n.T("Mode of launching Jenkins, you can choose: java, docker"))
+	flags.BoolVarP(&computerLaunchOption.Remove, "remove", "", false,
+		i18n.T("Automatically remove the container when it exits"))
+	flags.BoolVarP(&computerLaunchOption.Detach, "detach", "d", false,
+		i18n.T("Run container in background and print container ID"))
+	flags.StringVarP(&computerLaunchOption.Restart, "restart", "", "no",
+		i18n.T("Restart policy to apply when a container exits"))
+	flags.StringVarP(&computerLaunchOption.Type, "type", "", AgentJNLP,
 		i18n.T("The type of agent, include jnlp"))
-	computerLaunchCmd.Flags().BoolVarP(&computerLaunchOption.ShowProgress, "show-progress", "", true,
+	flags.StringVarP(&computerLaunchOption.AgentType, "agent-type", "", "",
+		i18n.T("The type of agent, include generic, maven, python. See also https://github.com/jenkinsci/jnlp-agents"))
+	flags.BoolVarP(&computerLaunchOption.ShowProgress, "show-progress", "", true,
 		i18n.T("Show the progress of downloading agent.jar"))
+
+	if err := computerLaunchCmd.RegisterFlagCompletionFunc("restart", common.ArrayCompletion("no", "always")); err != nil {
+		pluginCmd.PrintErrln(err)
+	}
+	if err := computerLaunchCmd.RegisterFlagCompletionFunc("mode", common.ArrayCompletion("java", "docker")); err != nil {
+		pluginCmd.PrintErrln(err)
+	}
+	if err := computerLaunchCmd.RegisterFlagCompletionFunc("agent-type", common.ArrayCompletion(
+		"generic", "maven", "python", "node", "ruby", "docker")); err != nil {
+		pluginCmd.PrintErrln(err)
+	}
 
 	healthCheckRegister.Register(getCmdPath(computerLaunchCmd), &computerLaunchOption)
 }
@@ -110,27 +139,72 @@ func (o *ComputerLaunchOption) LaunchJnlp(name string) (err error) {
 	if secret, err = o.ComputerClient.GetSecret(name); err == nil {
 		logger.Info("get agent secret", zap.String("value", secret))
 
-		var binary string
-		binary, err = util.LookPath("java", centerStartOption.LookPathContext)
-		if err == nil {
-			env := os.Environ()
-			agentArgs := []string{"java", "-jar", computerLaunchOption.Output,
-				"-jnlpUrl", fmt.Sprintf("%s/computer/%s/slave-agent.jnlp", o.ComputerClient.URL, name),
-				"-secret", secret, "-workDir", "/tmp"}
+		if o.Mode == "java" {
+			var binary string
+			binary, err = util.LookPath("java", centerStartOption.LookPathContext)
+			if err == nil {
+				env := os.Environ()
+				agentArgs := []string{"java", "-jar", computerLaunchOption.Output,
+					"-jnlpUrl", fmt.Sprintf("%s/computer/%s/slave-agent.jnlp", o.ComputerClient.URL, name),
+					"-secret", secret, "-workDir", "/tmp"}
 
-			if o.CurrentJenkins.ProxyAuth != "" {
-				proxyURL, _ := url.Parse(o.CurrentJenkins.Proxy)
-				agentArgs = append(agentArgs, "-proxyCredentials", o.CurrentJenkins.ProxyAuth)
+				if o.CurrentJenkins.ProxyAuth != "" {
+					proxyURL, _ := url.Parse(o.CurrentJenkins.Proxy)
+					agentArgs = append(agentArgs, "-proxyCredentials", o.CurrentJenkins.ProxyAuth)
 
-				proxyAuth := strings.SplitN(o.CurrentJenkins.ProxyAuth, ":", 2)
-				if len(proxyAuth) == 2 {
-					env = append(env, fmt.Sprintf("http_proxy=http://%s:%s@%s", url.QueryEscape(proxyAuth[0]), url.QueryEscape(proxyAuth[1]), proxyURL.Host))
+					proxyAuth := strings.SplitN(o.CurrentJenkins.ProxyAuth, ":", 2)
+					if len(proxyAuth) == 2 {
+						env = append(env, fmt.Sprintf("http_proxy=http://%s:%s@%s", url.QueryEscape(proxyAuth[0]), url.QueryEscape(proxyAuth[1]), proxyURL.Host))
+					}
 				}
+
+				logger.Debug("start a jnlp agent", zap.Any("command", strings.Join(agentArgs, " ")))
+
+				err = util.Exec(binary, agentArgs, env, o.SystemCallExec)
 			}
+		} else if o.Mode == "docker" {
+			var binary string
+			binary, err = util.LookPath("docker", centerStartOption.LookPathContext)
+			if err == nil {
+				env := os.Environ()
+				agentArgs := []string{"docker", "run", "--restart", o.Restart}
 
-			logger.Debug("start a jnlp agent", zap.Any("command", strings.Join(agentArgs, " ")))
+				if o.Remove {
+					agentArgs = append(agentArgs, "--rm")
+				}
 
-			err = util.Exec(binary, agentArgs, env, o.SystemCallExec)
+				if o.Detach {
+					agentArgs = append(agentArgs, "--detach")
+				}
+
+				var agentImage string
+				switch o.AgentType {
+				case "generic":
+					agentImage = "jenkins/slave:4.0.1-1-alpine"
+					agentArgs = append(agentArgs, []string{agentImage, "java", "-jar", "/usr/share/jenkins/agent.jar",
+						"-jnlpUrl", fmt.Sprintf("%s/computer/%s/slave-agent.jnlp", o.ComputerClient.URL, name),
+						"-secret", secret, "-workDir", "/tmp"}...)
+				case "maven", "python", "docker", "node", "ruby":
+					agentImage = fmt.Sprintf("jenkins/jnlp-agent-%s", o.AgentType)
+					agentArgs = append(agentArgs, []string{agentImage, "-url", o.ComputerClient.URL, secret, name}...)
+				}
+
+				if o.CurrentJenkins.ProxyAuth != "" {
+					proxyURL, _ := url.Parse(o.CurrentJenkins.Proxy)
+					agentArgs = append(agentArgs, "-proxyCredentials", o.CurrentJenkins.ProxyAuth)
+
+					proxyAuth := strings.SplitN(o.CurrentJenkins.ProxyAuth, ":", 2)
+					if len(proxyAuth) == 2 {
+						env = append(env, fmt.Sprintf("http_proxy=http://%s:%s@%s", url.QueryEscape(proxyAuth[0]), url.QueryEscape(proxyAuth[1]), proxyURL.Host))
+					}
+				}
+
+				logger.Debug("start a jnlp agent", zap.Any("command", strings.Join(agentArgs, " ")))
+
+				err = util.Exec(binary, agentArgs, env, o.SystemCallExec)
+			}
+		} else {
+			err = fmt.Errorf("not support mode: %s\n", o.Mode)
 		}
 	}
 	return
