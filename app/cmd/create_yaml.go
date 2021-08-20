2831065aec191b252af55eec4776c3116cc5f3d4
jxr <jqjxxjxr@126.com>
2021-08-20 12:37:53 +0800

    dsss

--- app/cmd/create_yaml.go
+++ app/cmd/create_yaml.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jenkins-zh/jenkins-cli/app/i18n"
 	"github.com/jenkins-zh/jenkins-cli/client"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 type YamlOption struct {
@@ -43,16 +44,22 @@ type coreAndPluginOption struct {
 }
 
 var coreAndPlugin coreAndPluginOption
+var yamlOption YamlOption
 
 func init() {
 	rootCmd.AddCommand(createYamlCmd)
 	createYamlCmd.Flags().StringArrayVarP(&coreAndPlugin.filter, "filter", "", []string{}, i18n.T("Filter for the list"))
 	createYamlCmd.Flags().BoolVarP(&coreAndPlugin.all, "all", "", false, i18n.T("Upgrade jenkins core and all plugins to update"))
 	createYamlCmd.Flags().BoolVarP(&coreAndPlugin.core, "core", "", false, i18n.T("Only upgrade jenkins core"))
+	createYamlCmd.Flags().StringVarP(&yamlOption.Bundle.GroupId, "bundle-groupId", "", "io.jenkins.tools.jcli.yaml.demo", i18n.T("GourpId of Bundle in yaml"))
+	createYamlCmd.Flags().StringVarP(&yamlOption.Bundle.ArtifactId, "bundle-artifactId", "", "jcli yaml demo", i18n.T("ArtifactId of Bundle in yaml"))
+	createYamlCmd.Flags().StringVarP(&yamlOption.Bundle.Vendor, "bundle-vendor", "", "jenkins-cli", i18n.T("Vendor of Bundle in yaml"))
+	createYamlCmd.Flags().StringVarP(&yamlOption.Bundle.Title, "bundle-title", "", "jcli create yaml demo", i18n.T("Title of Bundle in yaml"))
+	createYamlCmd.Flags().StringVarP(&yamlOption.Bundle.Description, "bundle-description", "", "Upgraded jenkins core and plugins in a YAML specification", i18n.T("Description of Bundle in yaml"))
 }
 
 var createYamlCmd = &cobra.Command{
-	Use:     "",
+	Use:     "create yaml",
 	Short:   "",
 	Long:    "",
 	Example: ``,
@@ -87,7 +94,11 @@ func (c *coreAndPluginOption) multipleChoice(cmd *cobra.Command, args []string) 
 	var yamlOption YamlOption
 	if c.all {
 		if _, pluginList, err := getLocalJenkinsAndPlugins(); err == nil {
+			yamlOption.Plugins = make([]Plugin, len(pluginList))
 			for index, plugin := range pluginList {
+				if index >= 5 {
+					break
+				}
 				yamlOption.Plugins[index].GroupId, yamlOption.Plugins[index].ArtifactId, yamlOption.Plugins[index].Source.Version, err = getGroupIdAndArtifactId(plugin.ShortName)
 				if err != nil {
 					return err
@@ -99,6 +110,7 @@ func (c *coreAndPluginOption) multipleChoice(cmd *cobra.Command, args []string) 
 				yamlOption.War.Source.Version = items[0].Title[8:]
 			}
 		}
+		renderBundle(cmd)
 	}
 	return nil
 }
@@ -119,7 +131,7 @@ func getGroupIdAndArtifactId(pluginName string) (groupId string, artifactId stri
 	if err != nil {
 		return "", "", "", err
 	}
-	version, groupId, artifactId = trimToId(pluginName)
+	version, groupId, artifactId = trimToId(newPluginOption.Version)
 
 	return groupId, artifactId, version, nil
 }
@@ -131,4 +143,20 @@ func trimToId(content string) (version string, groupId string, artifactId string
 	groupId = content[:endOfGroupId]
 	artifactId = content[endOfGroupId+1 : startOfVersionNumber]
 	return version, groupId, artifactId
+}
+
+func renderBundle(cmd *cobra.Command) (err error) {
+	bundle := Bundle{
+		yamlOption.Bundle.GroupId,
+		yamlOption.Bundle.ArtifactId,
+		yamlOption.Bundle.Vendor,
+		yamlOption.Bundle.Title,
+		yamlOption.Bundle.Description,
+	}
+	d, err := yaml.Marshal(&bundle)
+	if err != nil {
+		return err
+	}
+	cmd.Printf(string(d))
+	return nil
 }
