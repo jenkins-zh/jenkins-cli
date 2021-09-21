eee9d4eb749079aa8d6910f55694f6b4696126a8
jxr <jqjxxjxr@126.com>
2021-09-21 20:39:42 +0800

    modigy backup.go

--- app/cmd/backup.go
+++ app/cmd/backup.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/jenkins-zh/jenkins-cli/app/cmd/common"
 	"github.com/jenkins-zh/jenkins-cli/app/i18n"
@@ -12,6 +14,8 @@ import (
 //BackupOption is an option for backup
 type BackupOption struct {
 	RoundTripper http.RoundTripper
+	BackupDir    string
+	WaitTime     int
 }
 
 var backupOption BackupOption
@@ -19,6 +23,8 @@ var backupOption BackupOption
 func init() {
 	rootCmd.AddCommand(backupCmd)
 	healthCheckRegister.Register(getCmdPath(backupCmd), &backupOption)
+	backupCmd.Flags().StringVarP(&backupOption.BackupDir, "backup-dir", "d", "", i18n.T("the backup directory in thinBackup setting"))
+	backupCmd.Flags().IntVarP(&backupOption.WaitTime, "wait-time", "t", 300, i18n.T("the maximum time you would like to wait for jenkins to be idle and make a backup"))
 }
 
 var backupCmd = &cobra.Command{
@@ -47,11 +53,37 @@ func (o *BackupOption) Backup(cmd *cobra.Command, _ []string) (err error) {
 	}
 	GetCurrentJenkinsAndClient(&(jClient.JenkinsCore))
 	cmd.Println("Please wait while making a backup.")
-	_, err = jClient.RequestWithoutData(http.MethodGet, "/thinBackup/backupManual", nil, nil, 200)
+	err = ThinBackupAPI(jClient)
 	if err != nil {
-		cmd.Println("Backup failed. Please see logging message for detailed reasons.")
-	} else {
-		cmd.Println("Backup succeeds.")
+		cmd.Println("Backup failed. Please see logging message(/yourJenkinsHome/backup.log) for detailed reasons.")
 	}
+	i := 0
+	now := time.Now()
+	for o.WaitTime-i*15 > 0 {
+		time.Sleep(15 * time.Second)
+		stat, err := os.Stat(o.BackupDir)
+		if err != nil {
+			//check if the directory be modified and it is a hint that a backup may succeed
+			if stat.ModTime().After(now) {
+				modifyTime := stat.ModTime().Format("03:04:05")
+				hour := modifyTime[0:2]
+				min := modifyTime[3:5]
+				//check if thinBackup plugin createed the bakcup directory
+				_, err = os.Stat("*" + stat.ModTime().Format("2021-09-21") + "_" + hour + "-" + min)
+				if err == nil {
+					cmd.Println("Back up successfully!")
+					return nil
+				}
+			}
+		}
+
+	}
+	cmd.Println("Backup failed or thinBackup is still waiting for jenkins to be idle to make a backup. Please see logging message(/yourJenkinsHome/backup.log) for detailed reasons.")
 	return
+}
+
+//ThinBackupAPI requests backupManual api
+func ThinBackupAPI(client *client.CoreClient) (err error) {
+	_, err = client.RequestWithoutData(http.MethodGet, "/thinBackup/backupManual", nil, nil, 200)
+	return err
 }
